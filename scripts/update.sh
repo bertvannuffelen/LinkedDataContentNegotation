2781ff4b647d9ecb5f940160a265151095c7f02d
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-10-10 12:12:42 +0000

    move to new frontview 	modified:   config/httpd-vhosts.conf 	modified:   scripts/update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,7 +4,15 @@ REPOSITORYDIR=/data/oslo-vocabularia
 
 rm -rf $REPOSITORYDIR
 
-git clone --depth=1 -b draft https://github.com/Informatievlaanderen/OSLO-Vocabularia/ $REPOSITORYDIR
+git clone --depth=1 -b opmerkingen-bert https://github.com/Informatievlaanderen/OSLO-Vocabularia/ $REPOSITORYDIR
 
 #cp -r $REPOSITORYDIR/ns/* /www/ns
-cp -r $REPOSITORYDIR/context /www/context
+cp -r $REPOSITORYDIR/context /www
+cp -r $REPOSITORYDIR/doc/ap/* /www/doc/ap
+cp -r $REPOSITORYDIR/icons  /www
+cp -r $REPOSITORYDIR/images /www
+cp -r $REPOSITORYDIR/ns /www
+cp -r $REPOSITORYDIR/index.html /www/index.html
+cp -r $REPOSITORYDIR/*.pdf /www
+
+
