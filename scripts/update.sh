ce2187931c7570237cb26993f9acf08b1f52f803
Bert Van Nuffelen <Bert.vannuffelen@gmail.com>
2017-10-11 21:56:40 +0000

    get the master branch and align the rules

--- scripts/update.sh
+++ scripts/update.sh
@@ -4,11 +4,13 @@ REPOSITORYDIR=/data/oslo-vocabularia
 
 rm -rf $REPOSITORYDIR
 
-git clone --depth=1 -b opmerkingen-bert https://github.com/Informatievlaanderen/OSLO-Vocabularia/ $REPOSITORYDIR
+git clone --depth=1 -b master https://github.com/Informatievlaanderen/OSLO-Vocabularia/ $REPOSITORYDIR
 
 #cp -r $REPOSITORYDIR/ns/* /www/ns
+rm -rf /www/doc/ap
 cp -r $REPOSITORYDIR/context /www
-cp -r $REPOSITORYDIR/doc/ap/* /www/doc/ap
+cp -r $REPOSITORYDIR/cms/* /www
+cp -r $REPOSITORYDIR/doc /www
 cp -r $REPOSITORYDIR/icons  /www
 cp -r $REPOSITORYDIR/images /www
 cp -r $REPOSITORYDIR/ns /www
